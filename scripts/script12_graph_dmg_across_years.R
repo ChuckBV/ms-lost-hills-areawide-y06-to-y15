1b08118d02b42ff8da5314479bd5165de15e6981
ChuckBV <r.dominica@gmail.com>
2021-02-04 13:29:16 -0800

    updated 10 yr damage figures

--- scripts/script12_graph_dmg_across_years.R
+++ scripts/script12_graph_dmg_across_years.R
@@ -13,6 +13,7 @@
 
 library(tidyverse)
 library(lubridate)
+library(DescTools)
 
 ### Use huller data as coded/formatted for output to SAS
 huller_dmg <- read_csv("./data/huller_dmg_y06_to_y15_out_to_sas.csv")
@@ -93,7 +94,7 @@ p2
 
 #- 3. Damage by variety (NP v MO, huller data) -------------------------------
 
-windrow_interior <- read_csv("./data/windr") 
+windrow_interior <- read_csv("./data/windrow_interior_dmg_y06_to_y15_out_to_sas.csv") 
 
 var_by_yr2 <- windrow_interior %>% 
   filter(Variety %in% c("NP","MO")) %>% 
@@ -118,7 +119,9 @@ p2b <- ggplot(var_by_yr2, aes(fill=Variety, y=pctNOW, x=as.factor(Year))) +
         legend.position = c(0.25,0.75))
 p2b
 
-windrow_interior
+ggsave(filename = "y06_y15_windrow_interior_dmg.jpg", 
+       plot = p1, device = "jpg", path = "./results", 
+       dpi = 300, width = 5.83, height = 3.9, units = "in")  
 
 Desc(pctNOW ~ Variety, data = var_by_yr2)
 # ----------------------------------------------------------------------------#
@@ -135,7 +138,7 @@ Desc(pctNOW ~ Variety, data = var_by_yr2)
 # IQR       0.861    0.690
 # n            10       10
 # np      50.000%  50.000%
-#   NAs           0        0
+# NAs           0        0
 # 0s            0        0
 # 
 # Kruskal-Wallis rank sum test:
@@ -148,7 +151,7 @@ var_by_yr3 <- windrow_interior %>%
   group_by(Year,Variety) %>% 
   summarise(pctNOW = mean(pctNOW, na.rm = TRUE))
 
-var_by_yr3$Variety <- factor(var_by_y32$Variety, levels = c("NP","MO"))
+var_by_yr3$Variety <- factor(var_by_yr3$Variety, levels = c("NP","MO"))
 
 p2c <- ggplot(var_by_yr3, aes(fill=Variety, y=pctNOW, x=as.factor(Year))) + 
   geom_bar(position="dodge", stat="identity") +
@@ -167,30 +170,31 @@ p2c <- ggplot(var_by_yr3, aes(fill=Variety, y=pctNOW, x=as.factor(Year))) +
 p2c
 
 ggsave(filename = "y06_y15_dmg_np_vs_mo_in_insecticide.jpg", 
-       plot = p2c, device = "jpg", path = "./burks/results", 
+       plot = p2c, device = "jpg", path = "./results", 
        dpi = 300, width = 5.83, height = 3.9, units = "in")  
 
 
 Desc(pctNOW ~ Variety, data = var_by_yr3)
-# ---------------------------------------------------------------------------# 
+# --------------------------------------------------------------------------------------------------- 
 #   pctNOW ~ Variety (var_by_yr3)
 # 
 # Summary: 
 #   n pairs: 20, valid: 20 (100.0%), missings: 0 (0.0%), groups: 2
 # 
 # 
-# MO       NP
-# mean      1.008    1.435
-# median    0.638    1.004
-# sd        1.000    1.206
-# IQR       0.776    1.829
+#              NP       MO
+# mean      1.435    1.008
+# median    1.004    0.638
+# sd        1.206    1.000
+# IQR       1.829    0.776
 # n            10       10
 # np      50.000%  50.000%
-#   NAs           0        0
+# NAs           0        0
 # 0s            0        0
 # 
 # Kruskal-Wallis rank sum test:
 #   Kruskal-Wallis chi-squared = 0.46286, df = 1, p-value = 0.4963
+
 
 var_by_yr3
 
@@ -234,6 +238,6 @@ p1b <- ggplot(yr_avg2, aes(fill=Type, y=PctNOW, x=as.factor(Year))) +
 p1b
 
 ggsave(filename = "y06_y15_all_prod_dmg.jpg", 
-       plot = p1b, device = "jpg", path = "./burks/results", 
+       plot = p1b, device = "jpg", path = "./results", 
        dpi = 300, width = 5.83, height = 3.9, units = "in")  
 
