667de26b788a2a018717417c080dca6c3c109481
ChuckBV <r.dominica@gmail.com>
2021-02-04 11:12:41 -0800

    Updated to repl blocks instead of qtr sections

--- scripts/script11_compare_dmg_by_md_category.R
+++ scripts/script11_compare_dmg_by_md_category.R
@@ -31,26 +31,26 @@ library(FSA) # for dunnTest and se
 huller_dmg <- read_csv("./data/dmg_huller_y06_to_y15.csv")
 
 ### Treatments and categories
-# trts <- unique(huller_dmg$Treatment)
-# trts
+trts <- unique(huller_dmg$Treatment)
+trts
 # 
-# (chem <- trts[c(1,4)])
-# # [1] "C"    "Conv"
-# (md <- trts[c(3,5,7,9,11,14,15)])
-# # [1] "MD"      "1MD"     "2MD"     "50%MD"   "100%MD"  "EarlyMD" "StandMD"
-# (both <- trts[c(2,6,8,10,12,13,16)])
-# # [1] "CMD"    "1CMD"   "2CMD"   "50%C"   "100%C"  "EarlyC" "StandC"
+(chem <- trts[c(1,4)])
+# [1] "C"    "Conv"
+(md <- trts[c(3,5,7,9,11,14,15)])
+# [1] "MD"      "1MD"     "2MD"     "50%MD"   "100%MD"  "EarlyMD" "StandMD"
+(both <- trts[c(2,6,8,10,12,13,16)])
+# [1] "CMD"    "1CMD"   "2CMD"   "50%C"   "100%C"  "EarlyC" "StandC"
 # 
-# huller_dmg <- huller_dmg %>% 
-#   mutate(Trt_cat = case_when(
-#     Treatment %in% chem ~ "insecticide",
-#     Treatment %in% md ~ "mating_disruption",
-#     Treatment %in% both ~ "both",
-#     TRUE ~ "wtf"
-#   )) # filter finds no wtf
+huller_dmg <- huller_dmg %>% 
+  mutate(Trt_cat = case_when(
+        Treatment %in% chem ~ "insecticide",
+        Treatment %in% md ~ "mating_disruption",
+        Treatment %in% both ~ "both",
+        TRUE ~ "wtf"
+   )) # filter finds no wtf
 # 
-# huller_dmg %>% 
-#   filter(Trt_cat == "wtf")
+huller_dmg %>% 
+  filter(Trt_cat == "wtf")
 
 ### NB Two-part process. First char to numerical block codes, then block
 ### code to rep blocks
@@ -69,7 +69,7 @@ huller_dmg$Block2[is.na(huller_dmg$Block2)] <- 1.0
 
 huller_dmg$Block2 <- block_problems(huller_dmg$Block2)
 
-unique(huller_dmg$Block2)
+sort(unique(huller_dmg$Block2))
 
 ### Need to get rep data
 Rep_blocks <- read_csv("./data/lost_hills_arrangement.csv") %>% 
@@ -84,11 +84,8 @@ x <- huller_dmg %>%
   filter(is.na(Tier)) %>% 
   group_by(Year,Ranch2,Block2) %>% 
   summarise(nObs = n())
+x
   # 437 cases from 2008, Block2 = 1 (weight tickets)
-  # all other cases involve Blocks 11.3 or 25.3
-
-
-#-- STOPPED HERE ---------------------------------------##
 
 ### Output huller_dmg to sas
 write.csv(huller_dmg,
@@ -96,30 +93,30 @@ write.csv(huller_dmg,
           row.names = FALSE)
 
 hdmg2 <- huller_dmg %>% 
-  group_by(Year,Trt_cat,Ranch2,Block2,Variety,) %>% 
+  group_by(Year,Trt_cat,Tier,Variety,) %>% 
   summarise(pctNOW = mean(pctNOW, na.rm = TRUE))
 hdmg2
 
 Desc(pctNOW ~ Trt_cat, data = hdmg2)
-# --------------------------------------------------------------------------------------------------------- 
+# --------------------------------------------------------------------------------------------------- 
 #   pctNOW ~ Trt_cat (hdmg2)
 # 
 # Summary: 
-#   n pairs: 748, valid: 748 (100.0%), missings: 0 (0.0%), groups: 3
+#   n pairs: 301, valid: 301 (100.0%), missings: 0 (0.0%), groups: 3
 # 
 # 
-#                      both        insecticide  mating_disruption
-# mean                0.868              1.484              1.343
-# median              0.310              0.628              0.534
-# sd                  1.683              2.984              2.549
-# IQR                 0.684              1.335              1.164
-# n                     269                199                280
-# np                35.963%            26.604%            37.433%
+# both        insecticide  mating_disruption
+# mean                0.829              1.484              1.340
+# median              0.303              0.560              0.601
+# sd                  1.619              3.147              2.445
+# IQR                 0.641              1.124              1.090
+# n                     104                 89                108
+# np                34.551%            29.568%            35.880%
 # NAs                     0                  0                  0
-# 0s                      3                  0                  1
+# 0s                      1                  0                  0
 # 
 # Kruskal-Wallis rank sum test:
-#   Kruskal-Wallis chi-squared = 35.882, df = 2, p-value = 1.615e-08
+#   Kruskal-Wallis chi-squared = 19.104, df = 2, p-value = 7.107e-05
 
 hdmg2$Trt_cat <- factor(hdmg2$Trt_cat,
                         levels = c("insecticide","mating_disruption","both"))
@@ -130,15 +127,16 @@ PT
 # Dunn's test of multiple comparisons using rank sums : bonferroni  
 # 
 #                               mean.rank.diff    pval    
-# mating_disruption-insecticide      -33.20424  0.2923    
-# both-insecticide                  -114.17582 4.8e-08 ***
-# both-mating_disruption             -80.97159 3.4e-05 ***
+# mating_disruption-insecticide      -6.436278 1.00000    
+# both-insecticide                  -49.314661 0.00026 ***
+# both-mating_disruption            -42.878383 0.00101 ** 
 # ---
 # Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
 
+
 #-- 2. Compare interior damage ------------------------------------------------
 
-windrow_dmg <- read_csv("./burks/data-intermediate/dmg_wndrw_y06_to_y15.csv")
+windrow_dmg <- read_csv("./data/dmg_wndrw_y06_to_y15.csv")
 windrow_dmg
 # A tibble: 1,862 x 12
 #   date       type  plot     treatment variety ranch block position tot_nuts inf_now dmg_now sample_id
@@ -155,8 +153,7 @@ windrow_dmg <- windrow_dmg %>%
 
 windrow_interior <- windrow_dmg %>%  # equivalent to y from script 10
   filter(plot == "Interior") %>%
-  mutate(block = block_problems(block),
-         ranch = 10*ranch) %>% 
+  mutate(ranch = 10*ranch) %>% 
   group_by(Year,ranch,block,treatment,variety) %>% 
   summarise(pctNOW = mean(pctNOW, na.rm = TRUE))
 
@@ -215,6 +212,38 @@ windrow_interior <- windrow_interior %>%
 windrow_interior <- windrow_interior %>%
   filter(variety != "wtf") # removes 5 records w NAs in all vars that matter
 
+windrow_interior
+# A tibble: 789 x 8
+# Groups:   Year, ranch, block, treatment [275]
+#    Year ranch block treatment variety   pctNOW Trt_cat     Variety
+#    <dbl> <dbl> <dbl> <chr>     <chr>      <dbl> <chr>       <chr>  
+# 1  2006  3440  24.1 C         Fritz      1.39  insecticide FR     
+# 2  2006  3440  24.1 C         Monterrey  1.26  insecticide MO  
+
+### Compare variable names and make them compatible
+Rep_blocks <- read_csv("./data/lost_hills_arrangement.csv")
+
+Rep_blocks
+# A tibble: 33 x 4
+#    Ranch  Tier Block WestToEast
+#    <dbl> <dbl> <dbl>      <dbl>
+# 1  3450     1   2.2          1
+# 2  3450     1   2.1          2
+
+Rep_blocks <- Rep_blocks%>%
+  rename(ranch = Ranch,
+         block = Block)
+
+windrow_interior <- left_join(windrow_interior,Rep_blocks)
+windrow_interior
+# A tibble: 789 x 10
+# Groups:   Year, ranch, block, treatment [275]
+# Year ranch block treatment variety   pctNOW Trt_cat     Variety  Tier WestToEast
+#    <dbl> <dbl> <dbl> <chr>     <chr>      <dbl> <chr>       <chr>   <dbl>      <dbl>
+# 1  2006  3440  24.1 C         Fritz      1.39  insecticide FR          7          2
+# 2  2006  3440  24.1 C         Monterrey  1.26  insecticide MO          7          2
+
+
 ### Output windrow_edge to sas
 write.csv(windrow_interior,
           "./burks/data-intermediate/windrow_interior_dmg_y06_to_y15_out_to_sas.csv", 
@@ -222,29 +251,30 @@ write.csv(windrow_interior,
 
 
 interior2 <- windrow_interior %>% 
-  group_by(Year,Trt_cat,ranch,block,Variety,) %>% 
+  group_by(Year,Trt_cat,Tier,Variety,) %>% 
   summarise(pctNOW = mean(pctNOW, na.rm = TRUE))
 interior2
 
 Desc(pctNOW ~ Trt_cat, data = interior2)
-# pctNOW ~ Trt_cat (edge2)
+# --------------------------------------------------------------------------------------------------- 
+#   pctNOW ~ Trt_cat (interior2)
 # 
 # Summary: 
-#   n pairs: 789, valid: 782 (99.1%), missings: 7 (0.9%), groups: 3
+#   n pairs: 295, valid: 292 (99.0%), missings: 3 (1.0%), groups: 3
 # 
 # 
-#                      both        insecticide  mating_disruption
-# mean                0.916              1.932              1.845
-# median              0.342              0.642              0.777
-# sd                  1.761              4.338              4.962
-# IQR                 0.878              1.509              1.581
-# n                     285                200                297
-# np                36.445%            25.575%            37.980%
-# NAs                     3                  0                  4
-# 0s                     76                 24                 47
+# both        insecticide  mating_disruption
+# mean                0.930              1.957              1.928
+# median              0.374              0.730              0.907
+# sd                  1.405              4.311              5.359
+# IQR                 0.823              1.556              1.774
+# n                     101                 85                106
+# np                34.589%            29.110%            36.301%
+# NAs                     1                  0                  2
+# 0s                      9                  3                  7
 # 
 # Kruskal-Wallis rank sum test:
-#   Kruskal-Wallis chi-squared = 35.34, df = 2, p-value = 2.118e-08
+#   Kruskal-Wallis chi-squared = 15.739, df = 2, p-value = 0.0003823
 
 interior2$Trt_cat <- factor(interior2$Trt_cat,
                         levels = c("mating_disruption","insecticide","both"))
@@ -254,10 +284,10 @@ PT
 # 
 # Dunn's test of multiple comparisons using rank sums : bonferroni  
 # 
-#                               mean.rank.diff    pval    
-# insecticide-mating_disruption       6.902593  1.0000    
-# both-mating_disruption            -96.509162 7.0e-07 ***
-# both-insecticide                 -103.411754 1.9e-06 ***
+#                               mean.rank.diff   pval    
+# insecticide-mating_disruption      -1.221865 1.0000    
+# both-mating_disruption            -41.737997 0.0011 ** 
+# both-insecticide                  -40.516133 0.0033 ** 
 # ---
 # Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
 
@@ -265,22 +295,21 @@ PT
 
 windrow_edge <- windrow_dmg %>%  # equivalent to y from script 10
   filter(plot == "Edge") %>%
-  mutate(block = block_problems(block),
-         ranch = 10*ranch) %>% 
+  mutate(ranch = 10*ranch) %>% 
   group_by(Year,ranch,block,treatment,variety) %>% 
   summarise(pctNOW = mean(pctNOW, na.rm = TRUE))
 
 windrow_edge
-# A tibble: 794 x 6
-# Groups:   Year, ranch, block, treatment [280]
+# A tibble: 642 x 6
+# Groups:   Year, ranch, block, treatment [226]
 # Year ranch block treatment variety   pctNOW
-#   <dbl> <dbl> <dbl> <chr>     <chr>      <dbl>
-# 1  2006  3440  24.1 C         Fritz      1.39 
-# 2  2006  3440  24.1 C         Monterrey  1.26 
+#    <dbl> <dbl> <dbl> <chr>     <chr>      <dbl>
+# 1  2006  3440  24.1 C         Fritz      1.52 
+# 2  2006  3440  24.1 C         Monterrey  3.23 
 
 #windrow_edge$block <- block_problems(windrow_edge$block)
 windrow_edge %>% # changes 3 instances of 11.4
-  filter(block %in% c(11.4,25.2)) # comes up 0
+  filter(block %in% c(11.4,25.2)) # Three instances in 2007
 
 ### Very important--treatment categories
 trts2 <- sort(unique(windrow_edge$treatment))
@@ -325,37 +354,54 @@ windrow_edge <- windrow_edge %>%
 windrow_edge <- windrow_edge %>%
   filter(variety != "wtf") # removes 5 records w NAs in all vars that matter
 
+Rep_blocks <- read_csv("./data/lost_hills_arrangement.csv")
+
+Rep_blocks
+# A tibble: 33 x 4
+#    Ranch  Tier Block WestToEast
+#    <dbl> <dbl> <dbl>      <dbl>
+# 1  3450     1   2.2          1
+# 2  3450     1   2.1          2
+
+Rep_blocks <- Rep_blocks%>%
+  rename(ranch = Ranch,
+         block = Block)
+
+windrow_edge <- left_join(windrow_edge,Rep_blocks)
+windrow_edge
+
+
 ### Output windrow_edge to sas
 write.csv(windrow_edge,
-          "./burks/data-intermediate/windrow_edge_dmg_y06_to_y15_out_to_sas.csv", 
+          "./data/windrow_edge_dmg_y06_to_y15_out_to_sas.csv", 
           row.names = FALSE)
 
 
 edge2 <- windrow_edge %>% 
-  group_by(Year,Trt_cat,ranch,block,Variety,) %>% 
+  group_by(Year,Trt_cat,Tier,Variety,) %>% 
   summarise(pctNOW = mean(pctNOW, na.rm = TRUE))
 edge2
 
 Desc(pctNOW ~ Trt_cat, data = edge2)
-# -------------------------------------------------------------------------------------------------------------------------------------- 
+# --------------------------------------------------------------------------------------------------- 
 #   pctNOW ~ Trt_cat (edge2)
 # 
 # Summary: 
-#   n pairs: 635, valid: 630 (99.2%), missings: 5 (0.8%), groups: 3
+#   n pairs: 301, valid: 298 (99.0%), missings: 3 (1.0%), groups: 3
 # 
 # 
 # both        insecticide  mating_disruption
-# mean                2.292              2.971              3.576
-# median              0.942              1.479              1.511
-# sd                  4.275              6.264              6.307
-# IQR                 2.064              2.585              3.593
-# n                     223                177                230
-# np                35.397%            28.095%            36.508%
-# NAs                     2                  0                  3
-# 0s                     30                 10                 22
+# mean                2.247              2.886              3.644
+# median              1.009              1.582              2.219
+# sd                  3.204              5.462              5.287
+# IQR                 2.634              2.580              2.781
+# n                     105                 85                108
+# np                35.235%            28.523%            36.242%
+# NAs                     1                  0                  2
+# 0s                      5                  2                  2
 # 
 # Kruskal-Wallis rank sum test:
-#   Kruskal-Wallis chi-squared = 12.837, df = 2, p-value = 0.001631
+#   Kruskal-Wallis chi-squared = 11.34, df = 2, p-value = 0.003448
 
 
 edge2$Trt_cat <- factor(edge2$Trt_cat,
@@ -363,12 +409,9 @@ edge2$Trt_cat <- factor(edge2$Trt_cat,
 
 PT <- DescTools::DunnTest(pctNOW ~ Trt_cat, data = edge2, method = "bonferroni")
 PT
-# 
 # Dunn's test of multiple comparisons using rank sums : bonferroni  
 # 
-#                               mean.rank.diff    pval    
-# insecticide-mating_disruption       6.902593  1.0000    
-# both-mating_disruption            -96.509162 7.0e-07 ***
-# both-insecticide                 -103.411754 1.9e-06 ***
-# ---
-# Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+#                               mean.rank.diff   pval    
+# insecticide-mating_disruption      -17.55882 0.4797    
+# both-mating_disruption             -39.70952 0.0023 ** 
+# both-insecticide                   -22.15070 0.2343    
